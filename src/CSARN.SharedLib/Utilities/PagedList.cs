c7adec18dd1c1d73df59c67d64190a728739c673
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-11-21 12:32:16 +0300

    Added pagination handler to the SharedLib

---
+++ src/CSARN.SharedLib/Utilities/PagedList.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+
+namespace CSARN.SharedLib.Utilities
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; private set; }
+        public int TotalPages { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalItemsCount { get; private set; }
+
+        public bool HasPreviousPage
+        {
+            get => CurrentPage > 1 ? true : false;
+        }
+        public bool HasNextPage
+        {
+            get => CurrentPage < TotalPages ? true : false;
+        }
+
+        private PagedList(List<T> items, int itemsCount, int pageNumber, int pageSize)
+        {
+            TotalPages = (int)Math.Ceiling(itemsCount / (double)pageSize);
+            CurrentPage = pageNumber;
+            PageSize = pageSize;
+            TotalItemsCount = itemsCount;
+
+            AddRange(items);
+        }
+
+        /// <summary>
+        /// Used to form a pagedList from provided query and page parameters.
+        /// Does not work properly with MongoCollections' IQueryable implementation, use overrided method with IFindFluent instead.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static async Task<PagedList<T>> ToPagedListAsync(IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            var itemsCount = query.Count();
+            var items = await (query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync());
+
+            return new PagedList<T>(items, itemsCount, pageNumber, pageSize);
+        }
+
+        /// <summary>
+        /// Used to form a pagedList from provided query and page parameters.
+        /// Work with MongoCollections properly.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static async Task<PagedList<T>> ToPagedListAsync(IFindFluent<T, T> query, int pageNumber, int pageSize)
+        {
+            var itemsCount = query.CountDocuments();
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Limit(pageSize)
+                .ToListAsync();
+
+            return new PagedList<T>(items, (int)itemsCount, pageNumber, pageSize);
+        }
+    }
+}
---
+++ src/CSARN.SharedLib/ViewModels/Pagination/PageParametersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSARN.SharedLib.ViewModels.Pagination
+{
+    public class PageParametersViewModel
+    {
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = ViewModelsErrors.OutOfRange)]
+        public int PageNumber { get; set; } = 1;
+        [Range(minimum: 1, maximum: 50, ErrorMessage = ViewModelsErrors.OutOfRange)]
+        public int PageSize { get; set; } = 20;
+    }
+}
