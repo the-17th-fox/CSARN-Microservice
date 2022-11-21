565e0f9269bb76623581cdccb8767c0c6146c422
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-11-21 12:30:49 +0300

    Added common logging forms and custom exceptions to the SharedLib

---
+++ src/CSARN.SharedLib/Constants/CommonLoggingForms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSARN.SharedLib.Constants
+{
+    public abstract class CommonLoggingForms
+    {
+        public const string MicroserviceIsAboutToRun = "[{MsvcName}] is about to run.";
+        public const string RequiredParamsMissed = "Required parameters hasn't been provided: [{params}]";
+        public const string ExceptionForm = "Unhandled exception has occured: [{Name}]:[{message}]";
+    }
+}
---
+++ src/CSARN.SharedLib/Constants/CustomExceptions/InvalidParamsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSARN.SharedLib.Constants.CustomExceptions
+{
+    public class InvalidParamsException : Exception
+    {
+        public InvalidParamsException(string? message = "Required parameters weren't provided.") : base(message)
+        {
+        }
+    }
+}
